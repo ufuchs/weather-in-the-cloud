3288206bb88f5206189329b05bc890cbe857d900
ufuchs <ufuchs@gmx.com>
2013-07-06 22:23:30 +0200

    separated 'vendor' from 'config'

---
+++ weather-config.js
@@ -0,0 +1,39 @@
+/*jslint node:true*/
+
+'use strict';
+
+module.exports = {
+
+    production : {
+
+        dir : './public/weather',
+
+        files : {
+
+            names : {
+                weatherSvg : 'weather.svg',
+                unweatherPng : 'unweather.png',
+                weatherPng : 'weather.png'
+            },
+
+            quantity : {
+                kindle4nt : 1,
+                df3120 : 4
+            }
+
+        },
+
+        /* expire time in seconds of the output files*/
+        expires : 3600
+
+    },
+
+    templatesPool : {
+        dir : './templates',
+        devices : {
+            kindle4nt : '600x800.svg',
+            df3120 : '320x240.svg'
+        }
+    }
+
+};
